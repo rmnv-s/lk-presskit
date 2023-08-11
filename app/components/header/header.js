76775e31a2467c5014b38dd856862af5e5dbc48e
dev <remnev.dev@gmail.com>
2023-08-11 19:21:03 +0300

    fix(link): homepage

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -6,7 +6,7 @@ export default function Header() {
       <Link href="/">
         <SvgLogoLk className="opacity-50 mb-3 mt-5" />
       </Link>
-      <span className="uppercase text-[20px] text-white">press kit </span>
+      <span className="uppercase text-[20px] text-white ">press kit </span>
     </header>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 export default function Home() {
   return (
-    <main className="flex-1 flex items-center ">
-      <ul className="text-white grid grid-rows-2 gap-12 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-3">
+    <main className="flex-1 flex items-center max-sm:my-10">
+      <ul className="md:grid-cols-3 sm:grid-cols-2 max-sm:gap-10 lg:grid-cols-3 text-white grid grid-rows-2 gap-12 ">
         <li className="flex flex-col opacity-70 hover:opacity-100">
           <span className="text-base font-bold mb-3 ">01</span>
           <Link className="text-2xl uppercase leading-12" href="/">
@@ -30,7 +30,7 @@ export default function Home() {
         <li className="flex flex-col opacity-70 hover:opacity-100">
           <span className="text-base font-bold mb-3 ">05</span>
           <Link className="text-2xl uppercase leading-12" href="/catalogs">
-            Cotalogs
+            Catalogs
           </Link>
         </li>
         <li className="flex flex-col opacity-70 hover:opacity-100">
