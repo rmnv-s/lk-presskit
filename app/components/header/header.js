3b321b60e491098ed47081b4befe37a310586275
Dev <remnev.dev@gmail.com>
2023-08-20 18:34:36 +0300

    fix(logo): change main logo

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -1,12 +1,21 @@
 import SvgLogoLk from '@/components/assets/logo.icon';
+import Image from 'next/image';
+import logoMain from '../../../public/img/Logo_LK_main.png';
 import Link from 'next/link';
 export default function Header() {
   return (
     <header>
-      <Link href="/">
-        <SvgLogoLk className="opacity-50 mb-3 mt-5" />
+      <Link href='/'>
+        {/* <SvgLogoLk className="opacity-50 mb-3 mt-5" /> */}
+        <Image
+          className='opacity-50 mb-3 mt-5'
+          src={logoMain}
+          width={65}
+          alt='logo'
+          priority
+        />
       </Link>
-      <span className="uppercase text-[20px] text-white ">press kit </span>
+      <span className='uppercase text-[20px] text-white '>press kit </span>
     </header>
   );
 }
