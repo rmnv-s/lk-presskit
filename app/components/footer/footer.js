48bc11c9097b201a8ca1ca7003c5774eed807b12
Stas Remnev <remnev.dev@gmail.com>
2024-01-19 15:49:41 +0300

    fix: date footer

--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -1,10 +1,10 @@
-import SvgInstagram from '@/components/assets/icons/instagram.icon';
-import Link from 'next/link';
+import SvgInstagram from "@/components/assets/icons/instagram.icon";
+import Link from "next/link";
 export default function Footer() {
   return (
     <footer className=" flex justify-between border border-t border-0 border-black pb-10 pt-6">
       <p className="text-sm font-normal text-black opacity-60">
-        © 2023 Linda Kristel. <br />
+        © 2024 Linda Kristel. <br />
         All rights reserved.
       </p>
       <Link
