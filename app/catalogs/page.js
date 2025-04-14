872c104b7aa87a707411d37cf423e1e40646ae97
Stas <remnev.dev@gmail.com>
2025-04-14 14:09:23 +0300

    add: cataloge 2025

--- app/catalogs/page.js
+++ app/catalogs/page.js
@@ -7,11 +7,11 @@ import catalogue from "../../public/img/catalogue_it.jpg";
 import catalogueEng from "../../public/img/catalogue_en.jpg";
 
 export default function Cotalogs() {
-  return (
-    <main className="flex-1 flex flex-col">
-      <Heading text="Catalogs" />
+    return (
+        <main className="flex-1 flex flex-col">
+            <Heading text="Catalogs" />
 
-      <ul className="border-t border-b border-grey py-10 ">
+            {/* <ul className="border-t border-b border-grey py-10 ">
         <li className="grid grid-cols-2 border-b border-grey mb-10 pb-10   lg:grid-cols-2 max-sm:grid-cols-1">
           <div className="flex flex-col justify-between">
             <div className="max-sm:mb-6">
@@ -65,62 +65,36 @@ export default function Cotalogs() {
             alt="cataloge"
           />
         </li>
-      </ul>
-      <ul className=" py-10 ">
-        <li className="grid grid-cols-2 border-b border-grey mb-10 pb-10   lg:grid-cols-2 max-sm:grid-cols-1">
-          <div className="flex flex-col justify-between">
-            <div className="max-sm:mb-6">
-              <p className="text-l text-black mb-3">
-                LK catalog professional English
-              </p>
-              <span className="block text-s text-black mb-3">
-                {" "}
-                Size: 180x180
-              </span>
-              <a
-                className="text-white text-base hover:text-black "
-                href="/catalogs/LK_Catalogue_Prof_eng_180х180_web.pdf"
-                alt="alt text"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Preview
-              </a>
-            </div>
-          </div>
-          <Image src={catalogueEng} alt="catalogue" priority />
-          <LinkDownload
-            href="/catalogs/LK_Catalogue_Prof_eng_180х180_web.pdf"
-            alt="cataloge"
-          />
-        </li>
-
-        <li className="grid grid-cols-2 lg:grid-cols-2 max-sm:grid-cols-1">
-          <div className="flex flex-col justify-between">
-            <div className="max-sm:mb-6">
-              <p className="text-l text-black mb-3">LK catalog English</p>
-              <span className="block text-s text-black mb-3">
-                {" "}
-                Size: 180x180
-              </span>
-              <a
-                className="text-white text-base hover:text-black"
-                href="/catalogs/LK_Catalogue_eng_180х180_web.pdf"
-                alt="alt text"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Preview
-              </a>
-            </div>
-          </div>
-          <Image src={catalogueEng} alt="catalogue" priority />
-          <LinkDownload
-            href="/catalogs/LK_Catalogue_eng_180х180_web.pdf"
-            alt="cataloge"
-          />
-        </li>
-      </ul>
-    </main>
-  );
+      </ul> */}
+            <ul className=" py-10 ">
+                <li className="grid grid-cols-2 border-b border-grey mb-10 pb-10   lg:grid-cols-2 max-sm:grid-cols-1">
+                    <div className="flex flex-col justify-between">
+                        <div className="max-sm:mb-6">
+                            <p className="text-l text-black mb-3">
+                                LK catalog professional English
+                            </p>
+                            <span className="block text-s text-black mb-3">
+                                {" "}
+                                2025
+                            </span>
+                            <a
+                                className="text-white text-base hover:text-black "
+                                href="/catalogs/LK_Catalogue_eng_2025_web.pdf"
+                                alt="alt text"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Preview
+                            </a>
+                        </div>
+                    </div>
+                    <Image src={catalogueEng} alt="catalogue" priority />
+                    <LinkDownload
+                        href="/catalogs/LK_Catalogue_eng_2025_web.pdf"
+                        alt="cataloge"
+                    />
+                </li>
+            </ul>
+        </main>
+    );
 }
